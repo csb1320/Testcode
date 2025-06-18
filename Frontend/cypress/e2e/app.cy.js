5fb9e7ab14d6d20e0a9661ecc0a9ad48a12a4934
조성빈 <csb1320@naver.com>
2025-06-18 15:22:23 +0900

    fix: e2e 테스트 수정

--- Frontend/cypress/e2e/app.cy.js
+++ Frontend/cypress/e2e/app.cy.js
@@ -1,23 +1,40 @@
+const loginAndLogout = (username, password) => {
+  cy.wait(500);
+  cy.get("input[placeholder='Username']").type(username);
+  cy.get("input[placeholder='Password']").type(password);
+  cy.get("button").contains("로그인").click();
+  cy.contains(`${username}님! 대시보드에 오신 것을 환영합니다!`).should(
+    "be.visible"
+  );
+  cy.wait(500);
+  cy.contains("로그아웃").click();
+  cy.contains("Login").should("be.visible");
+};
+
 describe("App 통합 흐름 테스트", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
-  it("초기에는 로그인 문구가 보여야 한다", () => {
-    cy.contains("로그인이 필요합니다").should("be.visible");
-    cy.contains("대시보드에 오신 것을 환영합니다").should("not.exist");
+  it("초기에는 로그인 화면이 보여야 한다", () => {
+    cy.contains("Login").should("be.visible");
+    cy.get("input[placeholder='Username']").should("exist");
+    cy.get("input[placeholder='Password']").should("exist");
   });
 
-  it("로그아웃 버튼 클릭 시 로그인 문구가 다시 보여야 한다", () => {
-    cy.wait(500);
+  it("조성빈 로그인 시나리오", () => {
+    loginAndLogout("조성빈", "4242");
+  });
 
-    // 로그인 먼저 수행
-    cy.get("button").contains("로그인").click();
-    cy.wait(500);
+  it("정유열 로그인 시나리오", () => {
+    loginAndLogout("정유열", "3260");
+  });
 
-    cy.contains("로그아웃").click();
+  it("임두현 로그인 시나리오", () => {
+    loginAndLogout("임두현", "8088");
+  });
 
-    cy.contains("로그인이 필요합니다").should("be.visible");
-    cy.contains("대시보드에 오신 것을 환영합니다").should("not.exist");
+  it("김수민 로그인 시나리오", () => {
+    loginAndLogout("김수민", "9126");
   });
 });
